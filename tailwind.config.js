a93430e2d247b8abe79fc8306b6c7bab76beab44
Murage-Ivy <ivymurage2000@gmail.com>
2024-05-03 16:53:22 +0200

    assets import

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        yellow: "#FED100",
+        orange: "#FF5800",
+        white: "#FFFFFF",
+        "light-gray": "#AFAFAF",
+        "light-blue": "#71C6F9",
+        "dark-gray": "#333333",
+      },
+    },
   },
   plugins: [],
 };
