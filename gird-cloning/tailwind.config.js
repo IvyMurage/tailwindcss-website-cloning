f52d733a99ae58ccee371481fc2ea677c55db0e7
Murage-Ivy <ivymurage2000@gmail.com>
2024-05-04 11:42:00 +0200

    grid layout practice

--- gird-cloning/tailwind.config.js
+++ gird-cloning/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ECECED",
+        secondary: "#343536",
+      },
+      padding:{
+        "3px": "3px",
+        "5px": "5px",
+        "10px": "10px",
+        "6px": "6px",
+      }
+    },
   },
   plugins: [],
 };
