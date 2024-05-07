1bc220f67d44786be2e6ef747db8c99951f5a2ef
Murage-Ivy <ivymurage2000@gmail.com>
2024-05-07 20:27:30 +0200

    Add more changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
-      },
-      colors: {
-        yellow: "#FED100",
-        orange: "#FF5800",
-        white: "#FFFFFF",
-        "light-gray": "#AFAFAF",
-        "light-bluee": "#71C6F9",
-        "dark-gray": "#333333",
-        "secondary-gray": "#f1f2f2",
-      },
-      inset: {
-        neg: -2,
-        "neg10px": "-10px",
-      },
-      borderWidth: {
-        1: "1px",
-        3: "3px",
-      },
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+            },
+            colors: {
+                yellow: "#FED100",
+                orange: "#FF5800",
+                white: "#FFFFFF",
+                "light-gray": "#AFAFAF",
+                "light-bluee": "#71C6F9",
+                "dark-gray": "#333333",
+                "secondary-gray": "#f1f2f2",
+            },
+            inset: {
+                neg: -2,
+                neg10px: "-10px",
+            },
+            borderWidth: {
+                1: "1px",
+                3: "3px",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
